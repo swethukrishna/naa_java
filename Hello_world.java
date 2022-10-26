c1c6d48af6a40e2cf9ccdebce3332eef4d3563c5
swethu <pavushettyswethu@gmail.com>
2022-10-26 11:36:41 +0000

    end

--- Hello_world.java
+++ Hello_world.java
@@ -1,10 +1,5 @@
 public class Hello_world {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-  System.out.println("Hello world!");
-  System.out.println("Hello world!");
-  System.out.println("Hello world!");
   System.out.println("Hello world!");
   System.out.println("Hello world!");
     System.out.println("Hello world!");
